abf8a0f2d154add2a3e41083d833c53ac2e19559
JonnyNova <JonnyNova@users.noreply.github.com>
2019-05-20 09:53:06 -0400

    Swap away from transpilers since they aren't needed

--- Source/UtilityBelts/Harmony/Harmony_ShieldBelt.cs
+++ Source/UtilityBelts/Harmony/Harmony_ShieldBelt.cs
@@ -1,7 +1,6 @@
-using System;
 using System.Collections.Generic;
-using System.Reflection.Emit;
 using Harmony;
+using RimWorld;
 using Verse;
 
 namespace FrontierDevelopments.UtilityBelts.Harmony
@@ -14,32 +13,7 @@ namespace FrontierDevelopments.UtilityBelts.Harmony
             return comp == null || comp.enabled;
         }
 
-        private static IEnumerable<CodeInstruction> CheckFirst(IEnumerable<CodeInstruction> instructions, ILGenerator il, bool shieldActive, bool block)
-        {
-            var patchPhase = 0;
-                
-            foreach (var instruction in instructions)
-            {
-                // immediately check the comp if it should block damage / not display
-                if (patchPhase == 0)
-                {
-                    var continueLabel = il.DefineLabel();
-                    instruction.labels = new List<Label>(new []{ continueLabel });
-                        
-                    yield return new CodeInstruction(OpCodes.Ldarg_0);
-                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Harmony_ShieldBelt), nameof(IsEnabled), new Type[]{ typeof(ThingWithComps) }));
-                    yield return new CodeInstruction(block ? OpCodes.Brtrue : OpCodes.Brfalse, continueLabel);
-                    yield return new CodeInstruction(shieldActive ? OpCodes.Ldc_I4_1 : OpCodes.Ldc_I4_0);
-                    yield return new CodeInstruction(OpCodes.Ret);
-
-                    patchPhase = -1;
-                }
-                    
-                yield return instruction;
-            }
-        }
-
-        [HarmonyPatch(typeof(RimWorld.ShieldBelt), nameof(RimWorld.ShieldBelt.AllowVerbCast))]
+        [HarmonyPatch(typeof(ShieldBelt), nameof(ShieldBelt.AllowVerbCast))]
         public class Harmony_ShieldBelt_AllowVerbCast
         {
             public static bool Postfix(
@@ -53,28 +27,31 @@ namespace FrontierDevelopments.UtilityBelts.Harmony
                 return !IsEnabled(__instance) || __result;
             }
         }
-        
-        [HarmonyPatch(typeof(RimWorld.ShieldBelt), nameof(RimWorld.ShieldBelt.CheckPreAbsorbDamage))]
+
+        [HarmonyPatch(typeof(ShieldBelt), nameof(ShieldBelt.CheckPreAbsorbDamage))]
         public class Harmony_ShieldBelt_CheckPreAbsorbDamage
         {
-            [HarmonyTranspiler]
-            public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator il)
+            [HarmonyPrefix]
+            [HarmonyPriority(Priority.First + 200)]
+            public static bool ShouldAbsorbDamageWithToggle(out bool __result, ShieldBelt __instance)
             {
-                return CheckFirst(instructions, il, false, true);
+                __result = IsEnabled(__instance);
+                return __result;
             }
         }
 
-        [HarmonyPatch(typeof(RimWorld.ShieldBelt), "ShouldDisplay", MethodType.Getter)]
+        [HarmonyPatch(typeof(ShieldBelt), "ShouldDisplay", MethodType.Getter)]
         public class Harmony_ShieldBelt_ShouldDisplay 
         {
-            [HarmonyTranspiler]
-            public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator il)
+            [HarmonyPostfix]
+            public static bool ShouldDisplayWithToggle(bool __result, ShieldBelt __instance)
             {
-                return CheckFirst(instructions, il, false, true);
+                if (!IsEnabled(__instance)) return false;
+                return __result;
             }
         }
 
-        [HarmonyPatch(typeof(RimWorld.ShieldBelt), nameof(RimWorld.ShieldBelt.GetWornGizmos))]
+        [HarmonyPatch(typeof(ShieldBelt), nameof(ShieldBelt.GetWornGizmos))]
         public class Harmony_ShieldBelt_GetWornGizmos
         {
             public static IEnumerable<Gizmo> Postfix(IEnumerable<Gizmo> __result, RimWorld.ShieldBelt __instance)
